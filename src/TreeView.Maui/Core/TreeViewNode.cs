cbbd3cb10bdf5aebd8a4b51e86b5d55b904c2669
Enis Necipoglu <iletisim@enisnecipoglu.com>
2022-09-06 23:48:51 +0300

    Handled `Name`, `IsExtended` and `IsLeaf` properties

--- src/TreeView.Maui/Core/TreeViewNode.cs
+++ src/TreeView.Maui/Core/TreeViewNode.cs
@@ -1,10 +1,15 @@
 ﻿using System.Collections.ObjectModel;
+using System.Collections.Specialized;
 
 namespace TreeView.Maui.Core;
 
 public class TreeViewNode : BindableObject, ILazyLoadTreeViewNode
 {
     private bool? _isLeaf;
+    private IList<IHasChildrenTreeViewNode> children = new ObservableCollection<IHasChildrenTreeViewNode>();
+    private string name;
+    private bool isExtended;
+
     public TreeViewNode()
     {
     }
@@ -15,16 +20,29 @@ public class TreeViewNode : BindableObject, ILazyLoadTreeViewNode
         Value = value;
         IsExtended = isExtended;
 
-        if(children != null)
+        if (children != null)
         {
             Children = children;
         }
     }
 
-    public virtual string Name { get; set; }
-    public virtual bool IsExtended { get; set; }
+    public virtual string Name { get => name; set { name = value; OnPropertyChanged(); } }
+    public virtual bool IsExtended { get => isExtended; set { isExtended = value; OnPropertyChanged(); } }
     public virtual object Value { get; set; }
-    public virtual IList<IHasChildrenTreeViewNode> Children { get; set; } = new ObservableCollection<IHasChildrenTreeViewNode>();
+    public virtual IList<IHasChildrenTreeViewNode> Children { get => children; set { children = value; OnChildrenSet(); OnPropertyChanged(); } }
     public virtual Func<ITreeViewNode, IEnumerable<IHasChildrenTreeViewNode>> GetChildren { get; set; }
     public virtual bool IsLeaf { get => _isLeaf ?? !Children.Any() && GetChildren == null; set => _isLeaf = value; }
+
+    protected virtual void OnChildrenSet()
+    {
+        if (Children is INotifyCollectionChanged notifyCollectionChanged)
+        {
+            notifyCollectionChanged.CollectionChanged += (s, e) =>
+            {
+                OnPropertyChanged(nameof(IsLeaf));
+            };
+        }
+
+        OnPropertyChanged(nameof(IsLeaf));
+    }
 }
